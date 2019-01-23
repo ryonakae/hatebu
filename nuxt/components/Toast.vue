6d3d551ef6fb9373349423d1a1ee9296d1e0a484
Ryo Nakae <ryo.n.1129@gmail.com>
2019-01-23 16:34:34 +0900

    toast wip

---
+++ nuxt/components/Toast.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="toast" :class="{ 'is-visible': isVisible }">
+    <span class="toast-text">{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '',
+      isVisible: false
+    }
+  },
+
+  methods: {
+    async show(msg) {
+      this.msg = msg
+      this.isVisible = true
+    },
+
+    async hide(msg, delay) {
+      return new Promise(resolve => {
+        this.msg = msg
+        setTimeout(() => {
+          this.isVisible = false
+          resolve()
+        }, delay)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.toast {
+  position: fixed;
+  right: 0;
+  bottom: 20px;
+  left: 0;
+}
+</style>
