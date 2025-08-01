48a8361d172bad14ce56c0a470eb8a07684a5e89
Ryo Nakae <ryo.n.1129@gmail.com>
2025-08-01 18:41:38 +0900

    feat: 新しいスタイルシートとエラーページを追加
    - custom-properties.cssとcustom-media.cssを新規作成し、CSS変数とカスタムメディアクエリを定義
    - main.cssを新規作成し、基本的なスタイルを設定
    - error.vueを新規作成し、エラーメッセージを表示するコンポーネントを追加
    - package.jsonにressパッケージを追加

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,17 @@
 <template>
+  <h1>hatebu</h1>
   <NuxtPage />
 </template>
+
+<script>
+import '~/assets/styles/main.css'
+</script>
+
+<style scoped>
+.app {
+  max-width: var(--content-width);
+  margin-right: auto;
+  margin-left: auto;
+  background-color: var(--color-bg-content);
+}
+</style>
---
+++ app/error.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+  error: NuxtError
+}>()
+
+onMounted(() => {
+  console.error(props.error)
+})
+</script>
+
+<template>
+  <div class="error">
+    <div class="content">
+      <h1 class="title">
+        エラーが発生しました
+      </h1>
+      <NuxtLink
+        to="/"
+        class="link"
+      >俺のはてブ トップへ</NuxtLink>
+    </div>
+
+    <div class="detail">
+      <small>{{ error.statusCode }}</small>
+      <small>{{ error.message }}</small>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.error {
+  text-align: center;
+}
+
+.content {
+  position: fixed;
+  top: 50%;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: var(--padding-content-horizontal);
+  transform: translateY(-50%);
+}
+
+.title {
+  font-size: var(--fontsize-title);
+}
+
+.link {
+  margin-top: 1em;
+}
+
+.detail {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: var(--padding-content-horizontal);
+  color: var(--color-sub);
+}
+</style>
