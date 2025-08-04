17d2a649741f60742f561a5a81bd12764839b359
Ryo Nakae <ryo.n.1129@gmail.com>
2025-08-04 13:10:00 +0900

    feat: SEOメタ情報を追加し、アイコンファイルを追加
    - サイト情報を追加し、headとmeta情報を設定
    - apple-touch-icon、favicon、ogp画像を追加
    - robots.txtのUser-agentの表記を修正

--- app/app.vue
+++ app/app.vue
@@ -14,6 +14,36 @@
 const store = useCommonStore()
 const { start, finish } = useLoadingIndicator()
 
+const siteInfo = {
+  title: '俺のはてブ',
+  description: '俺のためのはてブ',
+  url: 'https://hatebu.brdr.jp',
+}
+
+// headとmeta情報を設定
+useHead({
+  htmlAttrs: {
+    lang: 'ja',
+  },
+  title: siteInfo.title,
+  link: [
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+    { rel: 'favicon', href: '/favicon.ico' },
+  ],
+})
+useSeoMeta({
+  ogTitle: siteInfo.title,
+  ogSiteName: siteInfo.title,
+  ogDescription: siteInfo.description,
+  ogUrl: siteInfo.url,
+  ogImage: siteInfo.url + '/ogp.png',
+  twitterCard: 'summary',
+  ogType: 'website',
+  ogLocale: 'ja_JP',
+})
+
+// store.loadingを監視
+// LoadingIndicatorを開始/終了する
 watch(
   () => store.loading,
   loading => loading ? start() : finish(),
